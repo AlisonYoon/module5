c80aaada6f440f10d862c703dcbe00cbf3928c34
AlisonYoon <politescarlett1@gmail.com>
2019-08-13 10:28:13 +0100

    JSON challange

--- json.js
+++ json.js
@@ -4,4 +4,10 @@ var person = {
     pet: 'Blob'
 }
 
-var json= JSON.stringify(person)
+var json= JSON.stringify(person)   // stringify() converts a value to JSON notation representing it:
+
+var jsonl = '{"name": "Sam", "age": 33, "pet": "Blob"}'
+
+var receivedData = JSON.parse(jsonl);
+
+console.log(receivedData)
