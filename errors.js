d08b00f77be525a2148d83874794bf53a41efb76
AlisonYoon <politescarlett1@gmail.com>
2019-08-13 15:04:06 +0100

    eval, URI error

--- errors.js
+++ errors.js
@@ -7,3 +7,5 @@ var boblin = 'bobling'
 //2. Type error : you misused a data type/accessed a property on a thing that doesn't exist(the thing, not the property)  (ie. console.log(undefined.name))
 //3. Reference error : access var that doesn't exist, or the var exist but accessing its property that doesn't exist.  (ie. someArray.lemgth -> lemgth property doesn't exist)
 //4. Range error :  if you try to use number that's too big for javascript
+//5. Eval error : eval() method error. but don't use eval() method anyway.
+//6. URI error : encodeURI() global function error. encodeURI(thing)   -> for example, space in string changed to '%20'
