52ab0a7ee0d2c3f4a734bc924e8a01184b375392
AlisonYoon <politescarlett1@gmail.com>
2019-08-13 13:25:08 +0100

    scope

---
+++ scope.js
@@ -0,0 +1,9 @@
+//scope : where we can get our stuff
+//PHP : functional scope.
+//      Have to pass as a parameter, have to return something if you want to access it
+
+
+//JS : Lexical scope with top down inheritance
+//     Each function creates its own scope, also it inherits its parent's scope
+//     Each function gets access to their parent's scope
+
