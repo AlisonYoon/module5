444512c74d51da6c9ce2850b896f2598d8a02ca1
AlisonYoon <politescarlett1@gmail.com>
2019-08-14 21:02:09 +0100

    trying to see if dynamically created element exists

--- shopping-cart/index.php
+++ shopping-cart/index.php
@@ -12,7 +12,7 @@
 <form action="index.php" method="post">
     <label for="item">Add item</label>
     <input type="text" name="item" id="item">
-    <button onclick="newElement()"></button>
+    <input type="submit">
 </form>
 <ul id="list">
 
--- shopping-cart/script.js
+++ shopping-cart/script.js
@@ -1,42 +1,48 @@
-var form = document.querySelector('form')
-var list = document.getElementById("list")
+var form = document.querySelector("form");
+var list = document.getElementById("list");
+var li = document.createElement("li");
+var btn = document.createElement("button");
 
+//https://ultimatecourses.com/blog/attaching-event-handlers-to-dynamically-created-javascript-elements
 
+form.addEventListener("submit", function(event) {
+  event.preventDefault();
+  var shoppingList = {};
+  shoppingList = form.item.value;
 
-// form.addEventListener('submit', function(event){
-//     event.preventDefault()
-//     var shoppingList = {}
-//     shoppingList = form.item.value
-//
-//     var json = JSON.stringify(shoppingList)
-//     list.innerHTML += '<li>' + json.replace(/['"]+/g, '')+ '<button class=\"delete\">delete</button></li>'
+  var json = JSON.stringify(shoppingList);
+  li.className = "dynamic-item";
+  list.appendChild(li);
+  li.innerHTML +=
+    json.replace(/['"]+/g, "") + '<button class="delete">delete</button>';
+  form.reset();
+});
+
+var deleteBtn = document.getElementByClassName("delete");
+console.log(deleteBtn);
+// function newElement(){
+//     var li = document.createElement("li")
+//     var inputValue = document.getElementById("item").value
+//     var t = document.createTextNode(inputValue)
+//     li.appendChild(t)
+
+//     if(inputValue === ''){
+//         alert("Please write something!")
+//     } else {
+//         list.appendChild(li)
+//     }
 //     form.reset()
-// })
 
+//     var span = document.createElement("span")
+//     var txt = document.createTextNode("\u00D7")
+//     span.className = "close"
+//     span.appendChild(txt)
+//     li.appendChild(span)
 
-function newElement(){
-    var li = document.createElement("li")
-    var inputValue = document.getElementById("item").value
-    var t = document.createTextNode(inputValue)
-    li.appendChild(t)
-
-    if(inputValue === ''){
-        alert("Please write something!")
-    } else {
-        list.appendChild(li)
-    }
-    form.reset()
-
-    var span = document.createElement("span")
-    var txt = document.createTextNode("\u00D7")
-    span.className = "close"
-    span.appendChild(txt)
-    li.appendChild(span)
-
-    for(var i = 0; i < close.length; i++){
-        close[i].onclick = function(){
-            var div = this.parentElement
-            div.style.display = "none"
-        }
-    }
-}
+//     for(var i = 0; i < close.length; i++){
+//         close[i].onclick = function(){
+//             var div = this.parentElement
+//             div.style.display = "none"
+//         }
+//     }
+// }
