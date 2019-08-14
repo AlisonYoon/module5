1f3291d2450edd499e4d5b9cb38ffd7921dd3b52
AlisonYoon <politescarlett1@gmail.com>
2019-08-14 10:37:48 +0100

    requestAnimationFrame()

--- web-api.js
+++ web-api.js
@@ -9,10 +9,44 @@
 //but don't stop the rest of the app from running
 setTimeout(function(){   // first param is the function, second param is the timeout
     console.log('ooooo')
-}, 2000)
+}, 2000)  // this is the minimum amount of time I'm asking it to evoke the function, but doesn't necessarily mean that it will happen exactly after 2 sec.
 
 console.log('eeee')    // this will print eeee
                         // ooooo
                         // because javascript is a single threaded language. it does one thing at a time
+
+
+var counter = 0
+var theTimer = setInterval(function(){   // this setInterval() function calls itself.
+    console.log(counter)
+    counter++
+}, 500)
+
+
+document.getElementById('click-me').addEventListener('click', function(){
+    clearInterval(theTimer)
+})
+
+
+
+//this is another web api that makes animation smoother. it doesn't run in constant time unless you define the time.
+requestAnimationFrame()
+
+var start = null;
+var element = document.getElementById('thing')
+
+function step(timestamp) {
+    if (!start) {
+        start = timestamp
+    }
+    var progress = timestamp - start
+    element.style.transform = 'translateX(' + Math.min(progress / 10, 200) + 'px)'
+    if (progress < 2000) {
+        requestAnimationFrame(step)
+    }
+}
+
+requestAnimationFrame(step)
+
 
 
