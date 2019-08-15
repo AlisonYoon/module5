24840dab5453f2429cdfd44a926fa1ff892d01ff
AlisonYoon <politescarlett1@gmail.com>
2019-08-15 11:36:28 +0100

    jquery exercises done

---
+++ jQuery-exercise/ajax.js
@@ -0,0 +1,38 @@
+var form = document.querySelector('form')
+
+form.addEventListener('submit', function(event){
+    event.preventDefault()   // it prevents the button to send input values as it normally would
+    var boxData = {}
+    boxData.name = form.name.value
+    boxData.petName = form.petName.value
+    boxData.favColour = form.favColour.value
+    boxData.favCar = form.favCar.value
+    boxData.goldeneye = form.goldeneye.value
+    boxData.marketing = form.marketing.value
+    boxData.recieveEmails = form.recieveEmails.value
+    boxData.email = form.email.value
+    boxData.age = form.age.value
+    boxData.marioKart = form.marioKart.value
+
+    var json = JSON.stringify(boxData)
+    //console.log(json)
+
+    //first exercise
+    // $.ajax('post.php/', {
+    //     method: 'post',
+    //     data: json,
+    //     success: function(responseData){
+    //         console.log(responseData)
+    //     }
+    // })
+
+    //second exercise
+    $.post('post.php/', json, function(responseData){
+        console.log(responseData)
+    })
+
+})
+
+//third exercise
+$('#optional').load('part.html')
+
