930b5b37bc8f0ad5e5d8595428a065312b62b9d4
AlisonYoon <politescarlett1@gmail.com>
2019-08-15 15:33:51 +0100

    max length 8 input works

--- validator-form/script.js
+++ validator-form/script.js
@@ -1,9 +1,18 @@
 var form = document.getElementById('validForm')
-var requiredInputValue = document.getElementById("requiredInput").value
 var requiredError = document.getElementById("requiredError")
+var maxLengthError = document.getElementById("maxLengthError")
 
 function validateFunction() {
-    if(requiredInputValue == ""){
+    var requiredInputValue = document.getElementById("requiredInput").value
+    var maxLengthValue = document.getElementById("maxLength").value
+    if(requiredInputValue === ""){
         requiredError.innerText = "Please fill this in"
+    } else {
+        requiredError.innerText = ""
+    }
+    if(maxLengthValue.length > 9){
+        maxLengthError.innerText = "Maximum 8 characters"
+    } else {
+        maxLengthError.innerText = ""
     }
 }
