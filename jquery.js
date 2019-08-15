752c5e2c6f83911c912156f07b8c5b1d8cadaeec
AlisonYoon <politescarlett1@gmail.com>
2019-08-15 09:40:59 +0100

    jquery is a javascript library

---
+++ jquery.js
@@ -0,0 +1,3 @@
+//jQuery is a JavaScript library
+//it gives you different way of writing things, different functions if you wanna use them
+//Framework has a different way of writing things and you must follow it
