861e416b567f51761d777eb66dc180e31cf3aecc
AlisonYoon <politescarlett1@gmail.com>
2019-08-16 09:02:14 +0100

    how do I make radio button not required?

--- validator-form/script.js
+++ validator-form/script.js
@@ -3,15 +3,20 @@ var requiredError = document.getElementById("requiredError")
 var maxLengthError = document.getElementById("maxLengthError")
 var minMaxLengthError = document.getElementById("minMaxLengthError")
 var minMaxLengthNotRequiredError = document.getElementById("minMaxLengthNotRequiredError")
+var lettersOnlyError = document.getElementById("lettersOnlyError")
+var letters = /^[A-Za-z]+$/;
+var radioYesRequiredError = document.getElementById("radioYesRequiredError")
 
 function validateFunction() {
     var requiredInputValue = document.getElementById("requiredInput").value
     var maxLengthValue = document.getElementById("maxLength").value
     var minMaxLengthValue = document.getElementById("minMaxLength").value
     var minMaxLengthNotRequiredValue = document.getElementById("minMaxLengthNotRequired").value
+    var lettersOnlyValue = document.getElementById("lettersOnly").value
+    var radioYesValue = document.form.required.value
 
     if(requiredInputValue === ""){
-        requiredError.innerText = "Please fill this in"
+        requiredError.innerText = "*Please fill this in"
     } else {
         requiredError.innerText = ""
     }
@@ -23,7 +28,7 @@ function validateFunction() {
     }
 
     if(minMaxLengthValue === "") {
-        minMaxLengthError.innerText = "This is required field"
+        minMaxLengthError.innerText = "*This is required field"
     } else {
         if(minMaxLengthValue.length < 11 || minMaxLengthValue.length > 26) {
             minMaxLengthError.innerText = "It should be at least 10 characters, maximum 25 characters"
@@ -32,9 +37,27 @@ function validateFunction() {
         }
     }
 
-    if(minMaxLengthNotRequiredValue.length < 11 || minMaxLengthNotRequiredValue.length > 26){
-        minMaxLengthNotRequiredError.innerText = "Enter at least 10 characters, maximum 25 characters"
-    } else {
+    if(minMaxLengthNotRequiredValue === "") {
         minMaxLengthNotRequiredError.innerText = ""
+    } else {
+        if(minMaxLengthNotRequiredValue.length < 11 || minMaxLengthNotRequiredValue.length > 26){
+            minMaxLengthNotRequiredError.innerText = "Enter at least 10 characters, maximum 25 characters"
+        } else {
+            minMaxLengthNotRequiredError.innerText = ""
+        }
     }
+
+
+    if(lettersOnlyValue === ""){
+        lettersOnlyError.innerText = ""
+    } else {
+        if(lettersOnlyValue.match(letters)){
+            lettersOnlyError.innerText = ""
+        } else {
+            lettersOnlyError.innerText = "Enter letters only"
+        }
+    }
+
+    console.log(radioYesValue)
+
 }
