9aaf44af229d6b39ec7bd6c1706687180971a93e
AlisonYoon <politescarlett1@gmail.com>
2019-08-20 09:11:51 +0100

    email validation works

--- validator-form/script.js
+++ validator-form/script.js
@@ -60,5 +60,15 @@ function validateFunction() {
         }
     }
 
+    if(emailValue){
+        var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        var emailValidate = re.test(String(emailValue).toLowerCase());
+        if(emailValidate){
+            emailError.innerText = ""
+        } else {
+            emailError.innerText = "Please type valid email"
+        }
+    }
+
 
 }  //validateFunction end
